b46902a39efb3eb76c7a161609491ed6cbd73942
Igor Zubarev <intactowow@gmail.com>
2024-04-22 20:31:24 +0300

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import { getYourName } from './cli.js';
 
-export const gameAttempt = 3;
-export const userName = getYourName();
-export const winGame = (userName, gameAttempt) => {
+const gameAttempt = 3;
+const userName = getYourName();
+const winGame = (userName, gameAttempt) => {
   if (gameAttempt < 1) {
     console.log(`Congratulations, ${userName}!`);
     return true;
   }
 }
-export const looseGame = (userName, gameAnswer, userAnswer) => {
+const looseGame = (userName, gameAnswer, userAnswer) => {
   if (gameAnswer !== userAnswer) {
     console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${gameAnswer}.`)
     console.log(`Let's try again, ${userName}!`);
 
     return true;
   }
+}
+
+export {
+  gameAttempt,
+  userName,
+  winGame,
+  looseGame
 }
