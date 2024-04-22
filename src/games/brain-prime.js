20be4722034db23f8d674bb81471a5c29914bd4c
Igor Zubarev <intactowow@gmail.com>
2024-04-22 14:58:10 +0300

    fix prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -9,7 +9,7 @@ export const brainPrime = (userName, gameAttempt) => {
   }
 
   if (gameAttempt === 3) {
-    print('Answer "yes" if the number is even, otherwise answer "no".');
+    print('Answer "yes" if given number is prime. Otherwise answer "no".');
   }
 
   const randomQuesValue = random();
